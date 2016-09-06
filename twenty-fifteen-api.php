1abd5c397f9f8ec5c68908da0abb572056044215
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-09-07 00:40:44 +0700

    init

---
+++ twenty-fifteen-api.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Twenty Fifteen API
+Plugin URI: http://magazine.enginethemes.com
+Description: API for frontend
+Version: 1.0
+Author: nguyenvanduocit
+Author URI: http://senviet.org
+License: GPL2
+*/

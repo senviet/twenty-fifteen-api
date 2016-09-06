83b290393cfc570b4380fb4fa2239deb79ad384a
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-09-07 01:08:09 +0700

    add setting endpoint

--- twenty-fifteen-api.php
+++ twenty-fifteen-api.php
@@ -9,3 +9,26 @@ Author: nguyenvanduocit
 Author URI: http://senviet.org
 License: GPL2
 */
+
+/**
+ * Register route
+ */
+function tfa_register_route(){
+	register_rest_route( 'wp/v2', '/settings', array(
+		'methods' => 'GET',
+		'callback' => 'tfa_get_setting',
+	) );
+}
+add_action( 'rest_api_init', 'tfa_register_route');
+
+/**
+ * Get site setting
+ *
+ * @return array
+ */
+function tfa_get_setting(){
+	return [
+		'title'=>get_bloginfo('name'),
+		'tagline'=>get_bloginfo('description')
+	];
+}

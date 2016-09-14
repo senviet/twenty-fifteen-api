a7d736de1294f70566a2dca0fec2e5a79c4b4b5e
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-09-15 00:19:49 +0700

    add date format

--- twenty-fifteen-api.php
+++ twenty-fifteen-api.php
@@ -10,33 +10,36 @@ Author URI: http://senviet.org
 License: GPL2
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-define( 'TFA_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define('TFA_DIR_PATH', plugin_dir_path(__FILE__));
 
-require_once  TFA_DIR_PATH . '/includes/tfa-api-menus.php';
+require_once TFA_DIR_PATH.'/includes/tfa-api-menus.php';
 
 /**
- * Register route
+ * Register route.
  */
-function tfa_register_route(){
-	register_rest_route( 'wp/v2', '/settings', array(
-		'methods' => 'GET',
-		'callback' => 'tfa_get_setting',
-	) );
+function tfa_register_route()
+{
+    register_rest_route('wp/v2', '/settings', array(
+        'methods' => 'GET',
+        'callback' => 'tfa_get_setting',
+    ));
 }
-add_action( 'rest_api_init', 'tfa_register_route');
+add_action('rest_api_init', 'tfa_register_route');
 
 /**
- * Get site setting
+ * Get site setting.
  *
  * @return array
  */
-function tfa_get_setting(){
-	return [
-		'title'=>get_bloginfo('name'),
-		'tagline'=>get_bloginfo('description')
-	];
+function tfa_get_setting()
+{
+    return [
+        'title' => get_bloginfo('name'),
+        'tagline' => get_bloginfo('description'),
+        'date_format' => get_option('date_format'),
+    ];
 }
